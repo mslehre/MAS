8c576f86153624f839ba3ce0e7a997aa7d4bb250
TimonKapischke <tk152210@uni-greifswald.de>
2019-04-09 16:14:17 +0200

    added Sequence Simulation files

---
+++ SequenceSimulation/main.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include "sequence.h"
+
+// This programm create one multiple fasta file with S sequences of lenght L. 
+// If there are more then one sequence then we have mutants with mutation probabilty P. 
+// Call skript with: ./main S L P
+// Exemple: ./main 5 100 0.3
+
+using namespace std;
+
+int main(int argc, char *argv[]){	
+	unsigned int numberOfSeqencens=atoi(argv[1]);
+	if(numberOfSeqencens<=0){
+		cerr << "\n The first number must be an positive integer.\n" << endl;
+		printHelp();
+		return -1;
+	}
+	unsigned int DNAlength=atoi(argv[2]);
+	if(DNAlength<=0){
+		cerr << "\n The second number must be an positive integer.\n"  << endl;
+		printHelp();
+		return -1;
+	}
+	double mutationpProbability=atof(argv[3]);
+	if(mutationpProbability<0 || mutationpProbability>1){
+		cerr << "\n The thirth number must be an double between 0 and 1.\n" << endl;
+		printHelp();
+		return -1;
+	}
+
+	vector<string> DNAsequences;
+	string DNA=randomDNA(DNAlength);
+	DNAsequences.push_back(DNA);
+
+	for(unsigned int i=1;i<=numberOfSeqencens-1;i++){
+		string mutantDNA=mutateDNA(DNA,mutationpProbability);
+		DNAsequences.push_back(mutantDNA);
+	}
+	
+	ofstream outfile("sequences.fa");
+		if(!outfile.is_open()){
+			cerr << "Error creating the output file." << endl;
+			return -1;
+		}
+
+		for(unsigned int i=0;i<DNAsequences.size();i++){
+			outfile << ">seq" << i+1;
+			for(unsigned int j=0;j<DNAlength;j++){
+				if(j%80==0){ 
+					outfile << "\n";
+				}
+    	  		outfile << DNAsequences[i][j];
+			}
+			outfile << endl;
+		}
+
+	outfile.close();
+
+return 0;
+}
+
+
+
+
+
---
+++ SequenceSimulation/sequence.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include "sequence.h"
+
+using namespace std;
+
+void printHelp(){
+	cout << "\t Call skript with:\t./makeFasta S L P \n" <<endl;
+	cout << "\t S: Must be an integer greater then zero." << endl;
+	cout << "\t\t S is the number of sequences that will created.\n" << endl;
+	cout << "\t L: Must be an integer greater then zero." << endl;
+	cout << "\t\t L is the lenght of the created sequences.\n" << endl;
+	cout << "\t P: Must be an double between zero and one." << endl;
+	cout << "\t\t P: is the probability for mutations in the sequencens.\n" << endl;
+}
+
+string randomDNA(unsigned int DNAlength){	
+	random_device rd;  				   // Will be used to obtain a seed for the random number engine
+	mt19937 gen(rd()); 				   // Standard mersenne_twister_engine seeded with rd()
+	uniform_int_distribution<> dis(0, 3); // Selects a number between 0 and 3
+
+	string alphabet = "ACTG";    
+	string DNA;  
+       
+	for(unsigned int i=0;i<=DNAlength;i++){
+		DNA=DNA+alphabet[dis(gen)];                    
+	}
+
+	return DNA;
+}
+
+string mutateDNA(string DNA, double mutationProbability){
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dis(0, 3);
+
+	string alphabet = "ACTG";
+	string mutantDNA=DNA;
+
+	for(unsigned int i=0;i<=DNA.length();i++){
+		double isMutant=((double) rand()/RAND_MAX); // random number between 0 and 1
+		if(isMutant<=mutationProbability){
+			mutantDNA[i]=alphabet[dis(gen)];
+		}
+	}
+
+	return mutantDNA;
+}
+
