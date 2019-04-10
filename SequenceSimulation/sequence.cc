5ac33d7c5ca16e17380e117dc4608b10fd8fa488
TimonKapischke <tk152210@uni-greifswald.de>
2019-04-10 18:51:07 +0200

    change comments

--- SequenceSimulation/sequence.cc
+++ SequenceSimulation/sequence.cc
@@ -18,9 +18,9 @@ void printHelp(){
 }
 
 string randomDNA(unsigned int DNAlength){	
-	random_device rd;  				  	  // Will be used to obtain a seed for the random number engine
-	mt19937 gen(rd()); 				  	  // Standard mersenne_twister_engine seeded with rd()
-	uniform_int_distribution<> dis(0, 3); // Selects a number between 0 and 3
+	random_device rd;	// Will be used to obtain a seed for the random number engine
+	mt19937 gen(rd());	// Standard mersenne_twister_engine seeded with rd()
+	uniform_int_distribution<> dis(0, 3);	// Selects a number between 0 and 3
 
 	string alphabet = "ACTG";    
 	string DNA;  
@@ -39,8 +39,8 @@ string mutateDNA(string DNA, double mutationProbability){
 	string alphabet = "ACTG";
 
 	for (unsigned int i = 1; i <= (DNA.length() + 1); i++) {
-		srand((time(NULL) * i));						// random seed for rand()
-		double isMutant = ((double) rand() / RAND_MAX); // random number between 0 and 1
+		srand((time(NULL) * i));	// random seed for rand()
+		double isMutant = ((double) rand() / RAND_MAX);	// random number between 0 and 1
 		if (isMutant <= mutationProbability)
 			DNA[i - 1] = alphabet[dis(gen)];
 	}
