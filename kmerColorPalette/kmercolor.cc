c77557b606372610657d35198aa645d6263b6132
Luc4sQ <pq164536@uni-greifswald.de>
2019-04-11 01:48:20 +0200

    implementation for 3-mer added

--- kmerColorPalette/kmercolor.cc
+++ kmerColorPalette/kmercolor.cc
@@ -1,18 +1,40 @@
 #include <iostream>
 #include <random>
 #include <string>
-#include <SFML/Graphics.hpp>
+#include <vector>
+#include <SFML/Graphics/Color.hpp>
 #include "kmercolor.h"
 
 using namespace std;
+using namespace sf;
+
+
+int charRead(char character){
+	if(character=='A'){
+		return 0;	
+	} else if(character == 'C'){
+		return 1;	
+	} else if(character == 'T'){
+		return 2;
+	} else if(character == 'G'){
+		return 3;
+	}
+}
 
 sf::Color kmerPatternColor(string kmer){
 	int kmerSize = kmer.size();
-	
-	if(kmerSize<=4){
+	Color returnValue(2,3,4,5);	
+	if(kmerSize==3){
 		//do something declared	
-		
-	
+		vector<int> characterMap;
+		characterMap.push_back(60);
+		characterMap.push_back(120);
+		characterMap.push_back(180);
+		characterMap.push_back(240);
+		returnValue.r = characterMap[charRead(kmer[0])];
+		returnValue.g = characterMap[charRead(kmer[1])];
+		returnValue.b = characterMap[charRead(kmer[2])];
+		return returnValue;
 	} else {
 		//do something automatically
 		
--- kmerColorPalette/main.cc
+++ kmerColorPalette/main.cc
@@ -5,6 +5,7 @@
 #include <vector>
 #include <stdlib.h>
 #include <SFML/Graphics.hpp>
+#include <cmath>
 #include "kmercolor.h"
 
 // This programm create one multiple fasta file with S sequences of lenght L. 
@@ -15,11 +16,18 @@
 using namespace std;
 
 int main(int argc, char *argv[]){	
-	string a = "hallo";
-	bool ex = a[1]=='a';
-	bool ey = a[1]=='v';	
-	cout << a[1] <<a[4]<<a.size()<<ex<<ey<<endl;
+	//string a = "hallo";
+	//bool ex = a[1]=='a';
+	//bool ey = a[1]=='v';	
+	//cout << a[1] <<a[4]<<a.size()<<ex<<ey<<endl;
+	long a = 255*255*255;
+	bool b;
+	for(int i=1;i<40;i++){
+		b = a>pow(4,i);
+		cout << b << " in the with k-mer size "<<i<< " with possible elements: "<<pow(4,i)<<endl;
+	}
 	cout << "hallo welt!" <<endl;
-	
+	sf::Color v = kmerPatternColor("CGC");
+	cout << (int)v.r << (int)v.g<<(int)v.r <<endl;
 }
 
