6d1d10817b7ac7eb57fbf6c43a34a30c6c8b25bf
mauricerad <mr123954@uni-greifswald.de>
2019-04-16 14:49:15 +0200

    worked on efficiency and style

--- AlignmentState/State.h
+++ AlignmentState/State.h
@@ -5,15 +5,21 @@
 #include <vector>
 #include <array>
 
+/** \brief state class encapsulates the current game state.
+ *         It can output possible Actions and select them.
+ */
+
 class state{
-	public:
-	state(std::vector <std::array<int,3>> e);//array wobei 1. Eintrag der index der i. sequence ist, 2. Eintrag index des k-mer in i. sequenz, 3. Eintrag index des k-mer in i+1. sequenz
-	state();
-	~state();
-	std::vector <std::array<int,3>> edges; 
-	std::vector <bool> consistentSubset; //shows whether edge is selected in current subset (true=selected)
-	std::vector <bool> selectable; //shows whether edge is selectable in the current subset (true=selectable)
-	void select(std::array<int,3> egde); //selects edge if selectable (does nothing otherwise)
-	void possibleActions(); //updates state after selecting an edge
+    public:
+    state(std::vector <std::array<int,3>> e);    ///< 1st entry is sequence nr, 2nd entry is node index in first seq, 3rd node index in 2nd seq
+    state();
+    ~state();
+    std::vector <std::array<int,3>> edges; 
+    std::vector <bool> selectedSubset;    ///< shows whether edge is selected in current subset (true=selected)
+    std::vector <bool> selectable;    ///< shows whether edge is selectable in the current subset (true=selectable)
+    void select(int i);    ///< selects edge i if selectable (does nothing otherwise)
+    void possibleActions();    ///< updates state after selecting an edge
+    bool checkConsistency(int i);   ///< checks whether edge is selectable while maintaining consistency in current subset
 };
 #endif
+
