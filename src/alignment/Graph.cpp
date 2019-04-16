1982baf290dd39b3c3f9426f350f12ec82d6d612
sebastianbierb <sebastian-bierbass@gmx.de>
2019-04-16 10:55:34 +0200

    einruecken mit leerzeichen statt tabs

--- src/alignment/Graph.cpp
+++ src/alignment/Graph.cpp
@@ -10,115 +10,115 @@ using namespace std;
 
 // constructor , store k-mer
 Graph::Graph(int km){
-	k = km;
+    k = km;
 }	
 
 // get StringListSequence
 vector<string>& Graph::getStringListSequence(){
-	return stringListSequence;
+    return stringListSequence;
 }
 
 // get number k of k-mer
 int Graph::getK(){
-	return k;
+    return k;
 }	
 
 // Method: get list of Edges
 vector<array<int,3>>& Graph::getEdgeList(){
-	return listOfEdges;
+    return listOfEdges;
 }
 
 // Method: reading fasta file
 void Graph::readingFastaFiles(std::string nameFile){
-	ifstream data (nameFile);										// open file			
+    ifstream data (nameFile);                                           // open file			
 																	
-	if (!data.is_open()) {
-		cout << "Can't open file!" << endl;
+    if (!data.is_open()) {
+        cout << "Can't open file!" << endl;
 	} else {
-		std::string line, name, content;
-		while (std::getline(data, line).good()) {
-			if (line.empty() || line[0] == '>') { 					// Identifier marker
-				if (!name.empty()) { 
-					stringListSequence.push_back(content);			// push sequence into vector
-					name.clear();
-				}
-				if (!line.empty())
-					name = line.substr(1);
+        std::string line, name, content;
+        while (std::getline(data, line).good()) {
+            if (line.empty() || line[0] == '>') {                       // Identifier marker
+                if (!name.empty()) { 
+                    stringListSequence.push_back(content);              // push sequence into vector
+                    name.clear();
+                }
+                if (!line.empty())
+                    name = line.substr(1);
 
-					content.clear();
-			} else if (!name.empty()) {
-						if (line.find(' ') != std::string::npos) { 	// Invalid sequence--no spaces allowed
-							name.clear();
-							content.clear();
-						} else {
-							content += line;
-						}
-					}
-		}
-		if (!name.empty())
-			stringListSequence.push_back(content); 					// push sequence into vector		
+                    content.clear();
+                } else if (!name.empty()) {
+                    if (line.find(' ') != std::string::npos) {          // Invalid sequence--no spaces allowed
+                        name.clear();
+                        content.clear();
+                    } else {
+                        content += line;
+                    }
+                }
+        }
+        if (!name.empty())
+            stringListSequence.push_back(content);                      // push sequence into vector		
 	}
-	calcEdgeList();
+    calcEdgeList();
 }
 
 // Method: get adjacent edges from node
 vector<int> Graph::getAdjacentEdge(int seq, int kNum){
-	vector<string>& stringList = getStringListSequence();			
-	vector<int> adjacentNode;
+    vector<string>& stringList = getStringListSequence();			
+    vector<int> adjacentNode;
 
-	if (seq == stringList.size()-1) {								// the last sequence has no next sequence
-		cerr << kNum << "It is the last sequence." << endl;
-		return adjacentNode;
-	}
+    if (seq == stringList.size()-1) {                                   // the last sequence has no next sequence
+        cerr << kNum << "It is the last sequence." << endl;
+        return adjacentNode;
+    }
 	
-	int kmer = getK(); 
-	int position = kmer * kNum;										
+    int kmer = getK(); 
+    int position = kmer * kNum;										
 
-	if (position + kmer <= stringList.at(seq).size()) {				
-		for (int i=0; i < stringList.at(seq+1).size(); i = i+kmer) {// iteration through the sequence
-			if (i+kmer > stringList.at(seq+1).size())
-				break;                         
-			if (stringList.at(seq).substr(position,kmer) == stringList.at(seq+1).substr(i,kmer))
-				adjacentNode.push_back(i/kmer);		
-		}
-		return adjacentNode;
-	} else {
-		cerr << kNum << ".k-mer don't exists in sequence " << seq  << "." << endl;
-		return adjacentNode; 
-	}	
-	cerr << kNum << ".k-mer don't exists in sequence " << seq  << "." << endl;
-	return adjacentNode;
+    if (position + kmer <= stringList.at(seq).size()) {				
+        for (int i = 0; i < stringList.at(seq+1).size(); i = i + kmer) {// iteration through the sequence
+            if (i+kmer > stringList.at(seq+1).size())
+                break;                         
+            if (stringList.at(seq).substr(position,kmer) == stringList.at(seq + 1).substr(i,kmer))
+                adjacentNode.push_back(i/kmer);		
+        }
+        return adjacentNode;
+    } else {
+        cerr << kNum << ".k-mer don't exists in sequence " << seq  << "." << endl;
+        return adjacentNode; 
+    }	
+    cerr << kNum << ".k-mer don't exists in sequence " << seq  << "." << endl;
+    return adjacentNode;
 }
 // Method: get Number of k-mer
 const vector<int>& Graph::getAmountOfKmer(){
-	vector<string>& stringList = getStringListSequence();
-	int sizeOfStringList = stringList.size();
+    vector<string>& stringList = getStringListSequence();
+    int sizeOfStringList = stringList.size();
 
-	if (!numberOfKmer.empty()) {
-		return numberOfKmer; 		
-	} else {
-		for (int i = 0; i < sizeOfStringList; i++) {
-			numberOfKmer.push_back(stringList.at(i).size() / getK());						
-		}
-	}
-	return numberOfKmer; 
+    if (!numberOfKmer.empty()) {
+        return numberOfKmer; 		
+    } else {
+        for (int i = 0; i < sizeOfStringList; i++) {
+            numberOfKmer.push_back(stringList.at(i).size() / getK());						
+        }
+    }
+    return numberOfKmer; 
 }					
 
 // Method: list of edges
 void Graph::calcEdgeList() {
-	vector<string>& stringList = getStringListSequence();
-	const vector<int>& amountKmer = getAmountOfKmer();
-	vector<int> adjEdges;
-	int j;
+    vector<string>& stringList = getStringListSequence();
+    const vector<int>& amountKmer = getAmountOfKmer();
+    vector<int> adjEdges;
+    int j;
 
-	for (int i = 0; i < stringList.size()-1; i++) {
-		for (j = 0; j < amountKmer.at(i); j++) {
-			adjEdges = getAdjacentEdge(i,j);
+    for (int i = 0; i < stringList.size()-1; i++) {
+        for (j = 0; j < amountKmer.at(i); j++) {
+            adjEdges = getAdjacentEdge(i,j);
 
-			for (auto &k : adjEdges) {
-			array<int,3> edge = {i,j,k};
-			listOfEdges.push_back(edge);
-			}
-		}	
-	}
+            for (auto &k : adjEdges) {
+            array<int,3> edge = {i,j,k};
+            listOfEdges.push_back(edge);
+            }
+        }	
+    }
 }
--- src/alignment/graphTest/main.cpp
+++ src/alignment/graphTest/main.cpp
@@ -6,28 +6,28 @@
 using namespace std;
 
 int main(int argc, char **argv){
-	// example create object from class Graph with 2-mer
-	Graph g(2);	
+    // example create object from class Graph with 2-mer
+    Graph g(2);	
 																		 
-	// reading fasta file
-	if (argc != 2) {
-		cerr << "Please start your program with: ./main [fileName.fa]" << endl;
-		return -1;
-	}
-	g.readingFastaFiles(argv[1]);							                        	
+    // reading fasta file
+    if (argc != 2) {
+        cerr << "Please start your program with: ./main [fileName.fa]" << endl;
+        return -1;
+    }
+    g.readingFastaFiles(argv[1]);							                        	
 	
-	cout << "Just an example: with " << g.getK() << "-mer.\n" <<  endl;
-	cout << "output sequences:" << endl;
-	// output sequences
-	for (const string &element : g.getStringListSequence()) {
-		cout << element << endl;
+    cout << "Just an example: with " << g.getK() << "-mer.\n" <<  endl;
+    cout << "output sequences:" << endl;
+    // output sequences
+    for (const string &element : g.getStringListSequence()) {
+        cout << element << endl;
 	}  	
 
-	for(const array<int,3>& element : g.getEdgeList()){
-		for(const int& a :element ){
+    for (const array<int,3>& element : g.getEdgeList()){
+        for(const int& a :element ){
 			cout << a << "  ";
 		}
-		cout << "" << endl;
-	}
-	return 0;
+        cout << "" << endl;
+    }
+    return 0;
 }
