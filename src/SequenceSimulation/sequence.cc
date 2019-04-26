6344264c59df41f6613d42a0f8676d69a36a5d97
TimonKapischke <tk152210@uni-greifswald.de>
2019-04-26 21:42:03 +0200

    New class Generator for random seed simulation

--- src/SequenceSimulation/sequence.cc
+++ src/SequenceSimulation/sequence.cc
@@ -1,8 +1,6 @@
-#include <iostream>
-#include <random>
-#include <string>
-#include <chrono>
 #include "sequence.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -19,46 +17,5 @@ void printHelp(){
     cout << "\t\t L is the length of the created sequences.\n" << endl;
     cout << "\t P: Must be an double between zero and one." << endl;
     cout << "\t\t P is the probability for mutations in the sequencens.\n" << endl;
-}
-
-/** 
- * \param DNAlength is the lenght of the DNA String that will returned
- * \return a random DNA String with length DNAlength
- */
-
-string randomDNA(unsigned int DNAlength){	
-    mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
-    uniform_int_distribution<> dis(0, 3); // selects a number between 0 and 3
-
-    string alphabet = "ACTG";    
-    string DNA;  
-       
-    for (unsigned int i = 0; i < DNAlength; i++) {
-        DNA = DNA + alphabet[dis(rng)];                    
-    }
-
-    return DNA;
-}
-
-/** 
- * \param DNA string that will be mutated
- * \param mutationProbability is the probability to mutate a character in the DNA string
- * \return a mutated DNA
- */
-
-string mutateDNA(string DNA, double mutationProbability){
-    mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
-    uniform_int_distribution<> dis(0, 3);
-    uniform_real_distribution<> dis2(0, 1);
-
-    string alphabet = "ACTG";
-
-    for (unsigned int i = 0; i < DNA.length(); i++) {
-        double isMutant = dis2(rng); // random number between 0 and 1
-        if (isMutant <= mutationProbability)
-            DNA[i] = alphabet[dis(rng)];
-    }
-
-    return DNA;
 }
 
--- src/SequenceSimulation/sequence.h
+++ src/SequenceSimulation/sequence.h
@@ -1,10 +1,6 @@
 #ifndef sequence_H_ 
 #define sequence_H_
 
-#include <string>
-
-void printHelp(); ///< print help
-std::string randomDNA(unsigned int DNAlength); ///< return a random DNA String with length DNAlength
-std::string mutateDNA(std::string DNA, double mutationProbability); ///< return a mutated DNA
+void printHelp();
 
 #endif //sequence_H_ 
