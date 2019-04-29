f7ccc76fc52ad5f44d2939ce160087616d706dad
MoritzGrillo <mg166257@uni-greifswald.de>
2019-04-29 14:56:07 +0200

    branch aktualisiert

---
+++ src/alignment/Edge.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Node.h"
+#include<string>
+#include<utility>
+
+using std::string;
+
+class Edge {
+    public:
+        Edge();             ///< default constructor Edge
+        Node first;         ///< first node of the edge
+        Node second;        ///< second node of the edge
+};
