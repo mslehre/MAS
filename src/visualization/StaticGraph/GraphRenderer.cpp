bb64003ac3b122c530ed5b067c17373d625ebe7c
Luc4sQ <pq164536@uni-greifswald.de>
2019-05-03 01:36:41 +0200

    fixed all visual issues (unefficient and just for the test) and implemented a 'scroll' function (arrow-buttons)

--- src/visualization/StaticGraph/GraphRenderer.cpp
+++ src/visualization/StaticGraph/GraphRenderer.cpp
@@ -3,57 +3,48 @@
 using namespace std;
 
 //ignore this, this is just a help method, which get solved later
-vector<string> giveKmers(vector<string>& Sequences, int k) {
-    vector<int> SequenceSizes; // A tupel of integers, where the i integer n stands for n Kmers in Sequencs i
-    // initialization of the SequenceSizes
-    for (unsigned int i = 0; i < Sequences.size(); i++)
-        SequenceSizes.push_back((int)floor((Sequences[i].size()) / k));
+vector<string> giveKmers(vector<Node>& Sequences) {
     vector<string> allKmers; // The set of all different Kmers
-    bool matched = false; // a boolean for the "Is this Kmer already in my set?"-Chec
-    int index; // a integer, which is index for the k-mers per sequence
-    /*
-     * here we go over the i Sequence and look at the index's Kmer and 
-     * push it into our set, when it is not already in.
-     */
-    for (unsigned int i = 0; i < Sequences.size(); i++) {
+    int index;
+    for (uint i = 0; i<Sequences.size(); i++) {
         index = 0;
-        while (index < SequenceSizes[i]) {
-            // comparing all k-mers in our set with the picked one
-            for (unsigned int j = 0; j < allKmers.size(); j++) {
-                if (allKmers[j] == Sequences[i].substr(index * k, k)) {
-                    matched = true;
-                    break;
-                }
+        for (uint j = 0; j<i; j++) {
+            if(Sequences[i].kmer == Sequences[j].kmer){
+                index = 1;
+                break;            
             }
-            // pushing a new Kmer
-            if (!matched)
-                allKmers.push_back(Sequences[i].substr(index * k, k));
-            matched = false;
-            index += 1;
+        }
+        if(index == 0) {
+            allKmers.push_back(Sequences[i].kmer);
         }
     }
     return allKmers;
 }
 
-GraphRenderer::GraphRenderer(){}
+GraphRenderer::GraphRenderer() { loaded = false;
+}
 
 void GraphRenderer::drawRectangle(sf::RenderWindow& window, int i, int j, sf::Color col, string kmer,
  sf::Font font, uint size){
-	sf::RectangleShape rectangle;
-	rectangle.setSize(sf::Vector2f(size, size/2));
-	rectangle.setFillColor(col);
-	rectangle.setPosition(size*0.2+(size*1.8)*j, size*0.2+((size/2)*1.8)*i);
-	window.draw(rectangle);
-    if(rectangles.size()!=i+1) {
-        vector<sf::RectangleShape> fill;
-        rectangles.push_back(fill);
+    if (!loaded){
+	    sf::RectangleShape rectangle;
+	    rectangle.setSize(sf::Vector2f(size, size/2));
+	    rectangle.setFillColor(col);
+	    rectangle.setPosition(size*0.2+(size*1.8)*j, size*0.2+((size/2)*1.8)*i);
+	    window.draw(rectangle);
+        while(rectangles.size()!=i+1 && rectangles.size()<10) {
+            vector<sf::RectangleShape> fill;
+            rectangles.push_back(fill);
+        }
+        rectangles[i].push_back(rectangle);
+    } else {
+        
+        window.draw(rectangles[i][j]);
     }
-    rectangles[i].push_back(rectangle);
-
 	
-	//Set the text for the kmers in the rectangle 
 	sf::Text text;
 	//Load the font from file
+	//Set the text for the kmers in the rectangle 
 	if(!font.loadFromFile("Amiko-Regular.ttf")){
 		std::cout <<"Can't find the font file" << std::endl;
 	}
@@ -62,11 +53,10 @@ void GraphRenderer::drawRectangle(sf::RenderWindow& window, int i, int j, sf::Co
 	text.setColor(sf::Color::Black);
 	text.setPosition(size*0.2+(size*1.8)*j+size*0.1, size*0.2+(size/2)*1.8*i+(size/2)*0.1);
 	// set the string to display
-	text.setString(kmer);
-			
+	text.setString(kmer);		
 	// set the character size
 	text.setCharacterSize(size*0.25);
-	window.draw(text);	
+	window.draw(text);
 }
 
  
@@ -98,12 +88,9 @@ void GraphRenderer::drawGraph(sf::RenderWindow& window, Graph& g, uint size){
 	sf::Font font;
     
     // get Graph information for the Kmerlist and Colorlist
-    int k = g.getK(); // integer with size of Kmer
-    vector<string> Sequences = g.getStringListSequence(); // vector with strings which stand for Sequences
-
-    // calculation of the different Kmers
-    vector<string> Kmers = giveKmers(Sequences, k); // vector of strings which stand for Kmers
-
+	vector<Node>& nodeList=g.getNodeList();
+    
+    vector<string> Kmers = giveKmers(nodeList);
     // initialize the colorlist
     colorlist colorExample(Kmers.size());
 
@@ -111,18 +98,15 @@ void GraphRenderer::drawGraph(sf::RenderWindow& window, Graph& g, uint size){
     vector<sf::Color> colors = colorExample.giveList(); // vector of different colors
     colormap mapExample(Kmers, colors);
 
-	//get the edges and nodes 
-	vector<Node>& nodeList=g.getNodeList();
 
-
-	//Visit every node in the nodelist	
-	for(uint i = 0; i<nodeList.size(); i++){
-		    drawRectangle(window, nodeList[i].i, nodeList[i].j, mapExample.Map(nodeList[i].kmer), 
-			nodeList[i].kmer, font, size);
-		if(i!=nodeList.size()-1 && nodeList.at(i).i==nodeList.at(i+1).i){
-		    drawLine(window, nodeList[i].i, nodeList[i].j , size);
-		}
-	}
+	//Visit every node in the nodelist
+	    for(uint i = 0; i<nodeList.size(); i++){
+		    drawRectangle(window, nodeList[i].i, nodeList[i].j, mapExample.Map(nodeList[i].kmer), nodeList[i].kmer, font, size);
+		    if(i!=nodeList.size()-1 && nodeList.at(i).i==nodeList.at(i+1).i){
+                drawLine(window, nodeList[i].i, nodeList[i].j , size);
+		    }
+	    }
+    loaded = true;
 }
 
 Node GraphRenderer::positionToNode(uint xpos, uint ypos, vector<Node>& nodeList, uint size){
@@ -150,18 +134,15 @@ bool GraphRenderer::isPositionNode(uint xpos, uint ypos, uint size){
 	}
 }
 
-void GraphRenderer::highlightRectangle(Node& node, sf::Color color, sf::RenderWindow& window, uint size){
+void GraphRenderer::highlightRectangle(Node& node, Graph& g, sf::RenderWindow& window, sf::Color color, uint size){
 	rectangles[node.i][node.j].setOutlineColor(color);
 	rectangles[node.i][node.j].setOutlineThickness(5);
-    
-	rectangles[node.i][node.j].setFillColor(sf::Color::Transparent);
-    /*sf::RectangleShape rectangle;
-	rectangle.setSize(sf::Vector2f(size, size/2));
-	rectangle.setFillColor(sf::Color::Transparent);
-	rectangle.setOutlineColor(color);
-	rectangle.setOutlineThickness(5);
-	rectangle.setPosition(size*0.2+(size*1.8)*node.j, size*0.2+((size/2)*1.8)*node.i);*/
-	window.draw(rectangles[node.i][node.j]);
+    drawGraph(window,g,size);
+}
+
+void GraphRenderer::removeRectangle(){
+    rectangles.clear();
+    loaded = false;
 }
 
 	
