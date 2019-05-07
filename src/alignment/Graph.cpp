b05c61b382fb827698e847e3d4fcf4ed22e7d2db
TimonKapischke <49186068+TimonKapischke@users.noreply.github.com>
2019-05-07 11:04:34 +0200

    Update Graph.cpp

--- src/alignment/Graph.cpp
+++ src/alignment/Graph.cpp
@@ -127,8 +127,7 @@ vector<int>& Graph::getNumberOfKmers(){
 // Method: list of edges
 void Graph::calcEdgeList() {
 
-    // calculate nodeList only with matches
-  
+    // calculate nodeList only with matches 
     for (unsigned int i = 0; i < nodeListAll.size() - 1; i++) {      
         calcAdjacentEdges(i);
     }
@@ -288,8 +287,6 @@ void Graph::calcNodeList() {
         for (j = 0; j < numberOfKmersTemp.at(i); j++) {
             if (stringList.at(i).length() - k >= j * k){
                 // store node in nodeListAll
-                nodeListAll.at(i).push_back(Node(i, j, stringList.at(i).substr(j * k, stringLength)));
-            }
-        }
+                 nodeListAll.at(i).push_back(Node(i, j, stringList.at(i).substr(j * k, stringLength)));           }
     }
 }
